8e1c40f415cfde1a10d87356b6be5d6783b46905
notrueblood <notrueblood@gmail.com>
2022-03-25 20:00:10 +0000

    Move db fetch to api

--- pages/analyze.tsx
+++ pages/analyze.tsx
@@ -4,17 +4,14 @@ import EventStatsTable, { EventCounts } from "components/kb/EventStatsTables";
 import IntangibleStatsTable, {
     IntangibleCounts,
 } from "components/kb/IntangiblesStatsTables";
-import { connectToDatabase } from "util/mongodb";
+import useSWR from "swr";
 
-function Page({
-    entityCounts,
-    eventCounts,
-    intangibleCounts,
-}: {
-    entityCounts: EntityCounts;
-    eventCounts: EventCounts;
-    intangibleCounts: IntangibleCounts;
-}) {
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+function Page() {
+    const { data, error } = useSWR(`/api/stats`, fetcher);
+    const { entityCounts, eventCounts, intangibleCounts } = data || {};
+
     return (
         <>
             <Box
@@ -26,65 +23,22 @@ function Page({
             >
                 <Container maxWidth="sm">
                     <Stack spacing={4}>
-                        <EntityStatsTable entityCounts={entityCounts} />
-                        <IntangibleStatsTable
-                            intangibleCounts={intangibleCounts}
-                        />
-                        <EventStatsTable eventCounts={eventCounts} />
+                        {entityCounts && (
+                            <EntityStatsTable entityCounts={entityCounts} />
+                        )}
+                        {intangibleCounts && (
+                            <IntangibleStatsTable
+                                intangibleCounts={intangibleCounts}
+                            />
+                        )}
+                        {eventCounts && (
+                            <EventStatsTable eventCounts={eventCounts} />
+                        )}
                     </Stack>
                 </Container>
             </Box>
         </>
     );
-}
-
-export async function getServerSideProps() {
-    const { db } = await connectToDatabase();
-
-    const gpeCount = await db.collection("entities.gpes").countDocuments({});
-    const logisticsServiceCount = await db
-        .collection("entities.logistics.services")
-        .countDocuments({});
-    const orgCount = await db.collection("entities.orgs").countDocuments({});
-    const productCount = await db
-        .collection("entities.products")
-        .countDocuments({});
-    const unitCount = await db.collection("entities.units").countDocuments({});
-
-    const sourceRequestCount = await db
-        .collection("events.source_requests")
-        .countDocuments({});
-
-    const classCount = await db
-        .collection("intangibles.classes")
-        .countDocuments({});
-    const offerCount = await db
-        .collection("intangibles.offers")
-        .countDocuments({});
-    const propertyCount = await db
-        .collection("intangibles.properties")
-        .countDocuments({});
-
-    return {
-        props: {
-            // JSON.parse(JSON.stringify()),
-            entityCounts: [
-                { name: "Geopolitical Entity", count: gpeCount },
-                { name: "Logistics Services", count: logisticsServiceCount },
-                { name: "Organizations", count: orgCount },
-                { name: "Products", count: productCount },
-                { name: "Units", count: unitCount },
-            ],
-            eventCounts: [
-                { name: "Source Request", count: sourceRequestCount },
-            ],
-            intangibleCounts: [
-                { name: "Class", count: classCount },
-                { name: "Offer", count: offerCount },
-                { name: "Property", count: propertyCount },
-            ],
-        },
-    };
 }
 
 export default Page;
---
+++ pages/api/stats.js
@@ -0,0 +1,48 @@
+import { connectToDatabase } from "util/mongodb";
+
+export default async function handler(req, res) {
+    const { db } = await connectToDatabase();
+
+    const gpeCount = await db.collection("entities.gpes").countDocuments({});
+    const logisticsServiceCount = await db
+        .collection("entities.logistics.services")
+        .countDocuments({});
+    const orgCount = await db.collection("entities.orgs").countDocuments({});
+    const productCount = await db
+        .collection("entities.products")
+        .countDocuments({});
+    const unitCount = await db.collection("entities.units").countDocuments({});
+
+    const sourceRequestCount = await db
+        .collection("events.source_requests")
+        .countDocuments({});
+
+    const classCount = await db
+        .collection("intangibles.classes")
+        .countDocuments({});
+    const offerCount = await db
+        .collection("intangibles.offers")
+        .countDocuments({});
+    const propertyCount = await db
+        .collection("intangibles.properties")
+        .countDocuments({});
+
+    const stats = {
+        // JSON.parse(JSON.stringify()),
+        entityCounts: [
+            { name: "Geopolitical Entity", count: gpeCount },
+            { name: "Logistics Services", count: logisticsServiceCount },
+            { name: "Organizations", count: orgCount },
+            { name: "Products", count: productCount },
+            { name: "Units", count: unitCount },
+        ],
+        eventCounts: [{ name: "Source Request", count: sourceRequestCount }],
+        intangibleCounts: [
+            { name: "Class", count: classCount },
+            { name: "Offer", count: offerCount },
+            { name: "Property", count: propertyCount },
+        ],
+    };
+
+    res.status(200).json(stats);
+}
